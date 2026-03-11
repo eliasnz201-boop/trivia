32613df221b2a7682ea321cf825f0ccfdd0ef531
Elias Nunez <eliasnz201@gmail.com>
2026-03-11 17:32:26 -0300

    se agreo un codigo

---
+++ trivia.py
@@ -0,0 +1,9 @@
+nombre = input("cual es tu nombre")
+print("Bienbenido a la trivia"(nombre))
+puntaje = 0
+respuesta = input("cual es la capital de Argentina?")
+if respuesta == "Buenos Aires":
+    puntaje = + 1
+respuesta = input("cuando se recuerda la independencia del Paraguay?")  
+   
+
